88940c59c17b30884426076d0f6c7dd425688a69
kkone <k.kone@panelys.com>
2024-12-07 14:46:45 +0000

    implement basic test with patrol

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -4,26 +4,31 @@ import 'package:patrol_basics_tutorial/main.dart';
 import 'package:patrol_basics_tutorial/pages/integration_test_keys.dart';
 
 void main() {
-  patrolTest(
-    'signs in, triggers a notification, and taps on it',
-    framePolicy: LiveTestWidgetsFlutterBindingFramePolicy.fullyLive,
-    ($) async {
-      initApp();
-      await $.pumpWidgetAndSettle(const MainApp());
-      await $(keys.signInPage.emailTextField).enterText('test@email.com');
-      await $(keys.signInPage.passwordTextField).enterText('password');
-      await $(keys.signInPage.signInButton).tap();
-      if (await $.native.isPermissionDialogVisible()) {
-        await $.native.grantPermissionWhenInUse();
-      }
-      await $(keys.homePage.notificationIcon).tap();
-      await $.native.pressHome();
-      await $.native.openNotifications();
-      await $.native.tapOnNotificationBySelector(
-        Selector(textContains: 'Patrol says hello!'),
-        timeout: const Duration(seconds: 5),
-      );
-      await $(keys.homePage.successSnackbar).waitUntilVisible();
-    },
-  );
+  patrolTest('signs in, triggers a notification, and tap on it',
+      framePolicy: LiveTestWidgetsFlutterBindingFramePolicy.fullyLive,
+      ($) async {
+    initApp();
+    await $.pumpWidgetAndSettle(const MainApp());
+    await $(keys.signInPage.emailTextField).enterText("test@gmail.com");
+    await $(keys.signInPage.passwordTextField).enterText("password");
+    await $(keys.signInPage.signInButton).tap();
+    if (await $.native.isPermissionDialogVisible()) {
+      await $.native.grantPermissionWhenInUse();
+    }
+
+    await $(keys.homePage.notificationIcon).tap();
+    await $.native.pressHome();
+    await $.native.openNotifications();
+    await $.native.tapOnNotificationBySelector(
+      Selector(textContains: 'Patrol says hello!'),
+      timeout: const Duration(
+        seconds: 5,
+      ),
+    );
+
+    await $(keys.homePage.successSnackbar).waitUntilVisible();
+
+    // await $(TextField).finder.
+    // await Future.delayed(const Duration(seconds: 2));
+  });
 }
